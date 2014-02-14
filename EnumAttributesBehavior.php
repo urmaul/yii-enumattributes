0f45b06ffe7fa2dad3f37d5ff7baf8cebb927c4a
urmaul <bogdan@itechcraft.com>
2014-02-14 18:54:10 +0200

    visible labels

--- EnumAttributesBehavior.php
+++ EnumAttributesBehavior.php
@@ -5,12 +5,18 @@
  * 
  * @property-read array $values
  * @property-read array $valueLabels
+ * @property-read array $visibleLabels
  * @property-read array $rule enum "in" validation rule.
  */
 class EnumAttributesBehavior extends CActiveRecordBehavior
 {
     public $attribute;
     public $labels;
+    /**
+     * Visible values array.
+     * @var string[]
+     */
+    public $visible;
     
     private $_cacheKey;
     
@@ -63,6 +69,20 @@ class EnumAttributesBehavior extends CActiveRecordBehavior
         
         return $labels;
     }
+    
+    public function getVisibleLabels()
+    {
+        $cName = 'valueLabels';
+        
+        if ( $this->_hasCache($cName) )
+            return $this->_getCache($cName, false);
+        
+        $labels = $this->getLabelsFor($this->visible);
+        
+        $this->_setCache($cName, $labels);
+        
+        return $labels;
+    }
 
     /**
      * Returns enum "in" validation rule.
@@ -71,6 +91,19 @@ class EnumAttributesBehavior extends CActiveRecordBehavior
     public function getRule()
     {
         return array($this->attribute, 'in', 'range' => $this->values);
+    }
+    
+    protected function getLabelsFor($values)
+    {
+        $labels = array();
+        foreach ($values as $value) {
+            if ( isset($this->labels[$value]) )
+                $labels[$value] = $this->labels[$value];
+            else
+                $labels[$value] = $this->owner->generateAttributeLabel($value);
+        }
+        
+        return $labels;
     }
     
     
