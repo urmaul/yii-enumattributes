05109f5f50112366e35fee4fd419ce2083856d64
urmaul <bogdan@itechcraft.com>
2014-02-14 18:56:04 +0200

    no more cache

--- EnumAttributesBehavior.php
+++ EnumAttributesBehavior.php
@@ -10,51 +10,38 @@
  */
 class EnumAttributesBehavior extends CActiveRecordBehavior
 {
+    /**
+     * Enum attribute name
+     * @var string
+     */
     public $attribute;
+    
+    /**
+     * Predefined labels
+     * @var string[]
+     */
     public $labels;
+    
     /**
      * Visible values array.
      * @var string[]
      */
     public $visible;
     
-    private $_cacheKey;
-    
-    protected static $_cache = array();
-    
-    
-    public function attach($owner)
-    {
-        parent::attach($owner);
-        
-        $this->_cacheKey = get_class($owner) . '.' . $this->attribute;
-    }
     
     public function getValues()
     {
-        $cName = 'values';
-        
-        if ( $this->_hasCache($cName) )
-            return $this->_getCache($cName, false);
-        
         $dbType = $this->owner->tableSchema->columns[$this->attribute]->dbType;
         $off = strpos($dbType, '(');
         $enum = substr($dbType, $off+1, -1);
         
         $values = str_replace("'", null, explode(',', $enum));
         
-        $this->_setCache($cName, $values);
-        
         return $values;
     }
     
     public function getValueLabels()
     {
-        $cName = 'valueLabels';
-        
-        if ( $this->_hasCache($cName) )
-            return $this->_getCache($cName, false);
-        
         $values = $this->values;
         $labels = array();
         
@@ -65,23 +52,12 @@ class EnumAttributesBehavior extends CActiveRecordBehavior
                 $labels[$value] = $this->owner->generateAttributeLabel($value);
         }
         
-        $this->_setCache($cName, $values);
-        
         return $labels;
     }
     
     public function getVisibleLabels()
     {
-        $cName = 'valueLabels';
-        
-        if ( $this->_hasCache($cName) )
-            return $this->_getCache($cName, false);
-        
-        $labels = $this->getLabelsFor($this->visible);
-        
-        $this->_setCache($cName, $labels);
-        
-        return $labels;
+        return $this->getLabelsFor($this->visible);
     }
 
     /**
@@ -97,33 +73,12 @@ class EnumAttributesBehavior extends CActiveRecordBehavior
     {
         $labels = array();
         foreach ($values as $value) {
-            if ( isset($this->labels[$value]) )
+            if (isset($this->labels[$value]))
                 $labels[$value] = $this->labels[$value];
             else
                 $labels[$value] = $this->owner->generateAttributeLabel($value);
         }
         
         return $labels;
-    }
-    
-    
-    /* Cache functions */
-    
-    protected function _hasCache($name)
-    {
-        return isset(self::$_cache[$this->_cacheKey], self::$_cache[$this->_cacheKey][$name]);
-    }
-    
-    protected function _getCache($name, $check = true)
-    {
-        if ( !$check || $this->_hasCache($name) )
-            return self::$_cache[$this->_cacheKey][$name];
-        else
-            return null;
-    }
-    
-    protected function _setCache($name, $value)
-    {
-        self::$_cache[$this->_cacheKey][$name] = $value;
     }
 }
